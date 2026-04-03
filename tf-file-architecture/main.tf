b5d5a120c1fbaa16b1d8011f5c90481aef89a939
Rinkesh <rinkesh.putta@xoriantcorp.onmicrosoft.com>
2026-04-03 08:59:35 +0530

    added terraform worflows execution commands

--- tf-file-architecture/main.tf
+++ tf-file-architecture/main.tf
@@ -12,10 +12,29 @@ resource "azurerm_resource_group" "example" {
   location = "Qatar Central"    #key = Value Pairs
 }
 
-resource "azurerm_resource_group" "$$$rinkesh$$$" {
+resource "azurerm_resource_group" "rinkesh" {
     name     = "cpg03-batch-rg03"   #key = Value Pairs  
     location = "South India"        #key = Value Pairs
 }
+
+
+resource "azurerm_resource_group" "kishore_rg" {
+    name = "cpg03-batch-kishore-rg"
+    location = "east us"
+}
+
+
+
+
+
+
+# resource "azurerm_resource_group" "user can edit" {
+#     user cannot edit      = "user can edit"   #key = Value Pairs  
+#     user cannot edit  = "user can edit"        #key = Value Pairs
+# }
+
+
+# Where we need the data type --> when user is inputting the data that is where we follow data Types else no data type
 
 
 
@@ -44,5 +63,44 @@ resource "azurerm_resource_group" "$$$rinkesh$$$" {
 
 
 
+
+
+***Terraform WorkFlows***
+
+1. terraform init --> To Download & Initialize the Provider Plugins and setup the modules
+
+2. terraform validate --> this command will check/validate the code Syntax and will let us know incase of any error's
+
+3. terraform plan --> This will generate the execution plan for the code you have written --> only Plan will be generated no action will be performed. 
+    + create (green color) --> this means it will create a new resources
+    - destroy (red color)  --> this will delete the resource
+    ~ modify (plain color) --> this will update or modify the resource
+    -/+  (Green & Red color Combination) --> This will destroy and re-create the resource due to main argument modifications
+
+
+4. terraform apply --> this will create or manage the resources on cloud or code deployment. It will create or mobdify/update or delete a resources in Cloud
+
+5. terraform destroy --> this will delete the entire infrastructure you have created using your terraform code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 */
+
+
+
 
--- tf-file-architecture/providers.tf
+++ tf-file-architecture/providers.tf
@@ -2,3 +2,19 @@
 # Either Azure or AWS or GCP or etc.,
 
 # Also, you can specify the backend state information
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+}
